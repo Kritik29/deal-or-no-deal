0f8cc53049be130e24ae05ac18c2e84dc883131f
Kritik29 <Kritik29@users.noreply.github.com>
2024-01-07 00:45:15 -0500

    work on case selection

--- deal_or_no_deal.py
+++ deal_or_no_deal.py
@@ -1,4 +1,9 @@
 import random
+import time
+import sys
+import locale
+
+locale.setlocale( locale.LC_ALL, '' )
 
 class bcolors:
     HEADER = '\033[95m'
@@ -23,6 +28,12 @@ DOLLAR_AMOUNTS = [0.01, 1, 5, 10, 25, 50, 75, 100, 200, 300, 400, 500, 750,
                   1000, 5000, 10000, 25000, 50000, 75000, 100000,
                   200000, 300000, 400000, 500000, 750000, 1000000]
 
+def slow_print(s):
+  for c in s:
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.05)
+
 def populateCases(d):
   # dictionary to store all the briefcases with dollar amounts in them
   cases = {}
@@ -35,9 +46,10 @@ def populateCases(d):
   
   return cases
 
-print('WELCOME TO DEAL OR NO DEAL!\n\nPick one of the 26 cases below\n')
+slow_print(f'\n\n{bcolors.HEADER}WELCOME TO DEAL OR NO DEAL!{bcolors.ENDC}\n\nPick one of the 26 cases below\n')
 cases_in_play = list(populateCases(DOLLAR_AMOUNTS).keys())
 print(*cases_in_play)
+print(populateCases(DOLLAR_AMOUNTS))
 USER_SELECTION = None
 
 while True:
@@ -50,7 +62,37 @@ while True:
   except ValueError:
     print("\nInvalid. Please choose a case number from the above\n")
 
-print(f"\n\nYou selected case: {bcolors.OKGREEN}{USER_SELECTION}{bcolors.ENDC}\n\n")
-print(f"{bcolors.YELLOW}Let's play Deal or No Deal!{bcolors.ENDC}\n\nYou will now select 6 cases to open\n\n")
+slow_print(f"\n\nYou selected case: {bcolors.OKGREEN}{USER_SELECTION}{bcolors.ENDC}\n\n")
+last_case_to_open = {USER_SELECTION: cases_in_play[USER_SELECTION-1]}
+print(last_case_to_open)
+del cases_in_play[USER_SELECTION-1]
+time.sleep(1)
+slow_print(f"{bcolors.YELLOW}Let's play Deal or No Deal!{bcolors.ENDC}\n\nYou will now select 6 cases to open.\n\n")
+
+case_to_open = None
+
+while True:
+  try:
+    case_to_open = int(input("\nWhat case do you want to open? "))
+    if (1 <= case_to_open <= 26) and case_to_open in cases_in_play:
+      break
+    else:
+      print("\nPlease select case between 1 and 26 that is in play.")
+  except ValueError:
+    print("\nInvalid. Please choose a case number from the above\n")
+
+slow_print(f"\n\nYou picked {bcolors.CYAN}Case Number {case_to_open}{bcolors.ENDC}. Case Number {case_to_open} had:")
+slow_print(f"\n\n {bcolors.YELLOW} {locale.currency(cases_in_play[case_to_open-1], grouping=True)} {bcolors.ENDC}")
+
+del cases_in_play[case_to_open-1]
+
+slow_print(f"\n\nThe cases remaining are: \n\n")
+print(*cases_in_play)
+
+## TODO: print the current board
+## TODO: need to consider dictionary element deletion, the order of the elements changes and this messes up the indexing
+## TODO: for some reason, accessing dictionary values from keys doesn't work as expected, need to debug
+
+
 
 
